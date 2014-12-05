42f2ef5f93592a7b603698f08c064efab5922b78
Infrabel <dotnet@infrabel.be>
2014-12-05 22:19:54 +0100

    Fixed NuGet Dependencies

--- nupack.cmd
+++ nupack.cmd
@@ -4,7 +4,7 @@ ECHO Packing GNaP.DependencyInjection
 nuget pack src\GNaP.DependencyInjection\GNaP.DependencyInjection.csproj -Build -Prop Configuration=Release -Exclude gnap.ico
 
 ECHO Packing GNaP.DependencyInjection.Web
-nuget pack src\GNaP.DependencyInjection.Web\GNaP.DependencyInjection.Web.csproj -Build -Prop Configuration=Release -Exclude gnap.ico
+nuget pack src\GNaP.DependencyInjection.Web\GNaP.DependencyInjection.Web.csproj -Build -Prop Configuration=Release -Exclude gnap.ico -IncludeReferencedProjects
 
 ECHO Packing GNaP.Core
-nuget pack src\GNaP.Core\GNaP.Core.csproj -Build -Prop Configuration=Release -Exclude gnap.ico
+nuget pack src\GNaP.Core\GNaP.Core.csproj -Build -Prop Configuration=Release -Exclude gnap.ico -IncludeReferencedProjects
